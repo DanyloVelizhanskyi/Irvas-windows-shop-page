cfbd654e1468231945cdb9bca7c69e8ef664c81d
Danylo Velizhanskyi <velizhanskiy1994@gmail.com>
2021-02-16 16:59:41 +0100

    adding tabs script

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -10,28 +10,28 @@ const modals = () => {
                     e.preventDefault();
                 }
     
-                modal.style.display = "block";
-                document.body.style.overflow = "hidden";
+                modal.style.display = 'block';
+                document.body.style.overflow = 'hidden';
             });
         });
 
         close.addEventListener('click', () => {
-            modal.style.display = "none";
-            document.body.style.overflow = "";
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
         });
 
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
-                modal.style.display = "none";
-                document.body.style.overflow = "";
+                modal.style.display = 'none';
+                document.body.style.overflow = '';
             }
         });
     }
 
     function showModalByTime(selector, time) {
         setTimeout(function() {
-            document.querySelector(selector).style.display = "block";
-            document.body.style.overflow = "hidden";
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow = 'hidden';
         }, time);
     }
 
